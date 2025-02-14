46677e669bfcbd57029ce9de9bb736de95caddaa
KaliDeveIoper <154928938+KaliDeveIoper@users.noreply.github.com>
2025-02-14 16:22:59 -0600

    Responsive
    Primer intento de hacer la pagina responsive

---
+++ js/hamburguesa.js
@@ -0,0 +1,34 @@
+var menu = document.querySelector('#menu-toggle');
+var drawer = document.querySelector('nav');
+var oscurecer = document.querySelector('.oscurecer');
+var enlaces = document.querySelectorAll('nav a'); // Seleccionar todos los enlaces dentro del nav
+
+menu.addEventListener('click', function (e) {
+    /* Abrir menú */
+    drawer.classList.toggle('active');
+
+    /* Oscurecer contenido al abrir el menú */
+    oscurecer.style.display = 'block';
+
+    /* Evitar que se haga scroll */
+    document.documentElement.style.overflow = "hidden";
+    e.stopPropagation();
+});
+
+oscurecer.addEventListener('click', function () {
+    cerrarMenu();
+});
+
+/* Agregar evento a cada enlace del menú */
+enlaces.forEach(function (enlace) {
+    enlace.addEventListener('click', function () {
+        cerrarMenu();
+    });
+});
+
+/* Función para cerrar el menú */
+function cerrarMenu() {
+    drawer.classList.remove('active');
+    oscurecer.style.display = 'none';
+    document.documentElement.style.overflow = "auto";
+}
