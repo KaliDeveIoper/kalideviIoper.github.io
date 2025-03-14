d4ddd6498a669c87963ef4c5703e7e2a5f376a14
KaliDeveIoper <154928938+KaliDeveIoper@users.noreply.github.com>
2025-03-13 20:11:28 -0600

    Cambios de logica en el slide

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,39 @@
 const slider = document.querySelector(".slider");
 const images = document.querySelectorAll(".slider img");
+const intervalo= 3000
 let distanciaDesplazamiento = images[0].offsetHeight;
-console.log(distanciaDesplazamiento)
-
-
 let index = 0;
 
-document.getElementById("next").addEventListener("click", () => {
-  if (index < images.length - 1) index++;
-  else index = 0;
-  slider.style.transform = `translateY(-${index * distanciaDesplazamiento}px)`;
-});
-
-document.getElementById("prev").addEventListener("click", () => {
-  if (index > 0) index--;
-  else index = images.length - 1;
-  slider.style.transform = `translateY(-${index * distanciaDesplazamiento}px)`;
-});
-
+let autoSlide= setInterval(avanzarSlider,intervalo)
 window.addEventListener("resize",calcularAlturaSlider);
+document.getElementById("next").addEventListener("click", () => avanzarSlider());
+document.getElementById("prev").addEventListener("click", () => retrocederSlider());
+
+document.querySelector(".slider").addEventListener("mouseenter", () => {
+   clearInterval(autoSlide);
+ });
+ document.querySelector(".slider").addEventListener("mouseleave", () => {
+   autoSlide = setInterval(avanzarSlider, intervalo);
+ });
+
+function avanzarSlider() {
+   if (index < images.length - 1) index++;
+   else index = 0;
+   slider.style.transform = `translateY(-${index * distanciaDesplazamiento}px)`;
+ }
+ 
+ function retrocederSlider() {
+   if (index > 0) index--;
+   else index = images.length - 1;
+   slider.style.transform = `translateY(-${index * distanciaDesplazamiento}px)`;
+ }
+
+
 
 function calcularAlturaSlider(){
    distanciaDesplazamiento = images[0].offsetHeight;
-   console.log(distanciaDesplazamiento)
    
 }
-
 
 function cambiarImagenesCon(){
    let imagenesCon =['img/placeHolderCuadrado.png','img/placeHolderCuadrado.png','img/placeHolderCuadrado.png']
@@ -34,16 +42,15 @@ function cambiarImagenesCon(){
    for (let i=0; i<images.length; i++){
       images[i].src=imagenesCon[i]
    }
-   slider.style.border = "1px solid var(--verdePastel)"
-   console.log("Se cambiaron las imagenes correctamente!!!")
+   
 }
 
 function cambiarImagenesSin(){
-   let imagenesSin =['img/placeHolderCuadrado.png','img/placeHolderCuadrado.png','img/placeHolderCuadrado.png']   
+   let imagenesSin =['img/sin1.jpg','img/sin2.jpg','img/sin3.jpg']   
    let indice=0
 
    for (let i=0; i<images.length; i++){
       images[i].src=imagenesSin[i]
    }
-   console.log("Se cambiaron las imagenes correctamente!!!")
+   
 }
